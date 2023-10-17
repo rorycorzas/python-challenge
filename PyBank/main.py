655dab83ffe41256e075ecdd53aab1cee9b3d23a
rorycorzas <134638444+rorycorzas@users.noreply.github.com>
2023-10-17 13:29:58 -0600

    Updated main.py from Pybank folder

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import os
 import csv
 from statistics import mean 
 
+
 file=("budget_data.csv")
 budget_csv = os.path.join("", "Resources",file)
 
@@ -14,8 +15,9 @@ gl =0
 date=[]
 profit=[]
 loss=[]
-pl = []
-i=1
+pl=[]
+delta_pl=[]
+i=0
 
 
 # Abre el archivo CSV
@@ -30,34 +32,34 @@ with open(budget_csv, newline='') as file:
     for each_line in csv_reader:
         date.append(each_line[0])
         profit_losses=int(each_line[1])
-        pl.append(each_line[1])
+        pl.append(int(each_line[1]))
+
         neto= neto + profit_losses
         
         if profit_losses>=0:
             profit.append(int(each_line[1]))
+
         else:
             loss.append(int(each_line[1]))        
-        
+
         #print(f"Date: {each_line[0]}, Profit/Losses: {each_line[1]} units")
         #print(f"Date: {date}, Profit/Losses: {profit_losses} units")
 
-#avr=mean(pl)
 months=int(len(date))
+gp=max(profit)
 
-#print(avr)
+
+for i in range(months-1):
+        delta_pl.append(pl[i+1]-pl[i])
+
+avr_change=round(mean(delta_pl),2)
+
+
+#print(gp.index)
+
+
+print(f"\nFinancial Analysis") 
+print(f"---------------------------------------------------")
 print(f"\nTotal Months: {months}") 
-print(f"\nTotal: ${neto}\n") 
-
-
-
-        #date_ls= string.split(separator, maxsplit)    
-
-        #print(f"Date: {date}, Profit/Losses: {profit_losses} units")
-
-
-#string.split(-,)      
-
-'''st_date = datetime.datetime(2018,9,16)
-end_date = datetime.datetime(2020,3,24)
-months = (end_date.year - st_date.year) * 12 + (end_date.month - st_date.month)
-print('The difference in months is: ',months)'''
+print(f"\nTotal: ${neto}") 
+print(f"\nAverage Change: ${avr_change}")
